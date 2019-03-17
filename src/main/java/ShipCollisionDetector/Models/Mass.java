df960e543a3b5590727772786d595c35d3a3c9ab
oviktor <viktor.odri123@gmail.com>
2019-03-17 17:41:04 +0100

    Add files via upload

--- src/main/java/ShipCollisionDetector/Models/Mass.java
+++ src/main/java/ShipCollisionDetector/Models/Mass.java
@@ -8,11 +8,26 @@ public class Mass implements Comparable<Mass> {
 	
 	private MassUnit massUnit;
 	private double massValue;
+
+	private double g;
+
+	private int t;
+
+	private double kg;
+
+	private double oz;
+
+	private double lb;
 	
-	public Mass(MassUnit massUnit, double massValue) {
+	public Mass(MassUnit massUnit, double massValue , double g, double kg, double t , double oz , double lb) {
 		//TODO: convert given unit into KG
 		this.massUnit = massUnit;
 		this.massValue = massValue;
+		this.setG(0.00001); 
+		   this.setKg(0.001);
+		this.setT(1);
+			this.setOz(0.00002834952);
+		this.setLb(0.000016);
 	}
 	
 	public MassUnit getMassUnit() {
@@ -30,6 +45,46 @@ public class Mass implements Comparable<Mass> {
 	public int compareTo(Mass otherMass) {
 		double otherMassValue = otherMass.getMassValue();
 		return (this.massValue - otherMassValue) < EPSILON ? 0 : (this.massValue < otherMassValue) ? -1 : 1;
+	}
+
+	public double getG() {
+		return g;
+	}
+
+	public void setG(double g) {
+		this.g = g;
+	}
+
+	public int getT() {
+		return t;
+	}
+
+	public void setT(int t) {
+		this.t = t;
+	}
+
+	public double getKg() {
+		return kg;
+	}
+
+	public void setKg(double kg) {
+		this.kg = kg;
+	}
+
+	public double getOz() {
+		return oz;
+	}
+
+	public void setOz(double oz) {
+		this.oz = oz;
+	}
+
+	public double getLb() {
+		return lb;
+	}
+
+	public void setLb(double lb) {
+		this.lb = lb;
 	}
 
 }
